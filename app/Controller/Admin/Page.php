d231436cb8dd7bf6d9d5ae3a8d96308a08ddef5a
CarlosEduardo99 <eduardopires99@gmail.com>
2023-10-06 16:14:28 -0400

    painel de administração

--- app/Controller/Admin/Page.php
+++ app/Controller/Admin/Page.php
@@ -19,4 +19,33 @@ class Page{
         ]);
     }
 
+    /**
+     * Método responsável por renderizar a view do menu do painel
+     * @param string $currentModule
+     * @return string
+     */
+    private static function getMenu($currentModule){
+        //RETORNA A RENDERIZAÇÃO DO MENU
+        return View::render('admin/menu/box',[
+
+        ]);
+    }
+
+    /**
+     * Método responsável por renderizar a view do painel com conteúdos dinâmicos
+     * @param string $title
+     * @param string $content
+     * @param string $currentModule
+     * @return string
+     */
+    public static function getPanel($title,$content,$currentModule){
+        //RENDERIZA A VIEW DO PAINEL
+        $contentPanel = View::render('admin/panel',[
+            'menu' => self::getMenu($currentModule),
+            'content' => $content
+        ]);
+        //RETORNA A PÁGINA RENDERIZADA
+        return self::getPage($title,$contentPanel);
+    }
+
 }
